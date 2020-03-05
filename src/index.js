4a153f24e2c515d7741da584f79068899eebfabf
Iliya Tryapitsin <iliya.tryapitsin@yandex.ru>
2020-03-05 13:32:57 +0200

    preserve member expression to string

--- src/index.js
+++ src/index.js
@@ -1,104 +1,53 @@
-import { declare } from "@babel/helper-plugin-utils";
+import {declare} from "@babel/helper-plugin-utils";
+import * as types from "@babel/types";
 
 const ignore = Symbol("no-preserve-to-string");
 
-function makePath(node) {
-    if (node.type === "MemberExpression") {
-        const path = `${makePath(node.object)}_${node.property.name}`;
-        return path;
-    } else if (node.type === "Identifier") {
-        return node.name;
+/**
+ * Return property navigation path by from lambda expression for member expression or node name for identifier
+ * @param node
+ * @returns {string|undefined}
+ */
+function getPathFromLambda(node) {
+    switch (node.type) {
+        case "MemberExpression":
+            return `${getPathFromLambda(node.object)}_${node.property.name}`;
+        case "Identifier":
+            return node.name;
+        default:
+            return undefined;
     }
 }
 
 export default declare(api => {
     api.assertVersion(7);
-    const { types: t } = api;
+    const {types: t} = api;
     return {
         name: "preserve-arrow-function-to-string",
         visitor: {
             ArrowFunctionExpression(path, state) {
-                if (!path.isArrowFunctionExpression()) return;
-                if (path.node[ignore]) return;
-                const desc = path.toString();
-                const id = path.scope.generateUidIdentifier("fn");
-                const selectorPath = makePath(path.node.body);
+                if (!path.isArrowFunctionExpression()) {
+                    return;
+                }
+                const isIgnore = path.node.leadingComments
+                    ? path.node.leadingComments.filter(comment => comment.value.indexOf("preserve-to-string") !== -1).length === 0
+                    : true;
+                if (isIgnore) {
+                    return;
+                }
+                const selectorPath = getPathFromLambda(path.node.body);
+                if (!selectorPath) return;
 
                 path.replaceWith(
-                    t.callExpression(
-                        t.functionExpression(
-                            null,
-                            [],
-                            t.blockStatement([
-                                // t.returnStatement(selectorPath)
-                            ])
-                        ),
-                        []
-                    )
+                    types.functionExpression(
+                        null,
+                        [],
+                        types.blockStatement([
+                            types.returnStatement(types.stringLiteral(selectorPath))
+                        ])
+                    ),
+                    []
                 );
-            },
-            VariableDeclarator(path, state) {
-                const bindingName = t.isImportDeclaration(path.node)
-                    ? path.node.specifiers[0].local.name
-                    : path.node.id.name;
-                const idxBinding = path.scope.getOwnBinding(bindingName);
-
-                idxBinding.constantViolations.forEach(refPath => {
-                    throw state.file.buildCodeFrameError(
-                        refPath.node,
-                        "`idx` cannot be redefined."
-                    );
-                });
-
-                let didTransform = false;
-                let didSkip = false;
-
-                idxBinding.referencePaths
-                    .slice()
-                    .reverse()
-                    .forEach(refPath => {
-                        if (refPath.node === idxBinding.node) {
-                            // Do nothing...
-                        } else if (refPath.parentPath.isCallExpression()) {
-                            // visitIdxCallExpression(refPath.parentPath, state);
-                            didTransform = true;
-                        } else {
-                            // Should this throw?
-                            didSkip = true;
-                        }
-                    });
-
-                if (!path.isArrowFunctionExpression()) return;
-                if (path.node[ignore]) return;
-                const desc = path.toString();
-                const id = path.scope.generateUidIdentifier("fn");
-                const node = {
-                    ...path.node,
-                    [ignore]: true
-                };
-                const toString = t.functionExpression(
-                    null,
-                    [],
-                    t.blockStatement([t.returnStatement(t.stringLiteral(desc))])
-                );
-                const all = t.functionExpression(
-                    null,
-                    [],
-                    t.blockStatement([
-                        t.variableDeclaration("const", [
-                            t.variableDeclarator(id, node)
-                        ]),
-                        t.expressionStatement(
-                            t.assignmentExpression(
-                                "=",
-                                t.identifier(`${id.name}.toString`),
-                                toString
-                            )
-                        ),
-                        t.returnStatement(id)
-                    ])
-                );
-                path.replaceWith(t.callExpression(all, []));
             }
         }
     };
